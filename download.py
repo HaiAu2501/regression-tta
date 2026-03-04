bbaba63ed6c381c45448448a96f6e4fe111cd980
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2026-03-04 18:55:39 +0700

    update

--- download.py
+++ download.py
@@ -2,13 +2,12 @@ import kagglehub
 from torchvision import datasets
 
 # MNIST
-datasets.MNIST(root="./data/MNIST", train=True, download=True)
-datasets.MNIST(root="./data/MNIST", train=False, download=True)
+# datasets.MNIST(root="./data/MNIST", train=True, download=True)
+# datasets.MNIST(root="./data/MNIST", train=False, download=True)
 
 # SVHN
-datasets.SVHN(root="./data/SVHN", split="train", download=True)
-datasets.SVHN(root="./data/SVHN", split="test", download=True)
+# datasets.SVHN(root="./data/SVHN", split="train", download=True)
+# datasets.SVHN(root="./data/SVHN", split="test", download=True)
 
 # UTKFace
-kagglehub.dataset_download("jangedoo/utkface-new", path="./UTKFace", output_dir="./data/UTKFace")
-
+# kagglehub.dataset_download("jangedoo/utkface-new", output_dir="./data/UTKFace")
